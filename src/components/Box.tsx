e06f35a71ae151dc46d8df1f43c4a499eef64974
awqsomee <awqsome@bk.ru>
2024-02-04 18:11:33 +0300

    refactor: replace react elements with styled

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 
-export const Box = ({ children }: { children: React.ReactNode }) => {
-  return <Layout>{children}</Layout>;
-};
-
-const Layout = styled.div`
+export const Box = styled.div<{ fd?: "column" | "row"; ai?: "center"; jc?: "center" | "space-between" }>`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  flex-direction: ${({ fd }) => fd || "column"};
+  align-items: ${({ ai }) => ai || "center"};
+  justify-content: ${({ jc }) => jc || "center"};
   width: 100%;
   height: 100%;
 `;
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,14 +2,11 @@ import React from "react";
 import styled from "styled-components";
 import { HeaderHeigth } from "./Header";
 
-export const Container = ({ children }: { children: React.ReactNode }) => {
-  return <Layout>{children}</Layout>;
-};
-
-const Layout = styled.div`
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 100vw;
+  padding: 0 264px;
   height: calc(100vh - ${HeaderHeigth});
 `;
