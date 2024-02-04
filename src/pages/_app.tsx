1fdf4856d32654a5d87116690a15bf9f5882d424
awqsomee <awqsome@bk.ru>
2024-02-04 17:33:29 +0300

    fix: change fonts import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,6 @@ import { ThemeProvider, createGlobalStyle } from "styled-components";
 import { Header } from "../components/Header";
 
 const GlobalStyles = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
-
 html,
 body {
     padding: 0;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,11 @@ import StyledComponentsRegistry from "../lib/registry";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Lato&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <StyledComponentsRegistry>
           <Main />
