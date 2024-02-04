6311c57c6d02b46ac69eff213fdede769e2a4ec2
awqsomee <awqsomee@users.noreply.github.com>
2024-02-04 13:44:24 +0000

    deploy: b6b124bee4ae30e933989de71fc08af196aab483

---
+++ _next/static/piHXdTQhT0pQyRJZ5Hufc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-2ea8fd41b14cabd0.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],"/[news]":["static/chunks/pages/[news]-52a2298e81eb5a05.js"],sortedPages:["/","/_app","/_error","/[news]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
